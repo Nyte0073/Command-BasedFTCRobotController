98696713656d83c40b07338b813faab10a8582db
Nyte0073 <808563@pdsb.net>
2025-05-27 18:57:44 -0400

    Committing latest code (May 27th, 2025)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboticsInterfaces/Commands/SwerveDriveCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboticsInterfaces/Commands/SwerveDriveCommand.java
@@ -8,23 +8,36 @@ import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.teamcode.RoboticsInterfaces.Subsystems.SwerveDrive;
 
 public class SwerveDriveCommand extends CommandBase {
+    /**Reference to the robot swerve drivetrain - access to all four wheels and to methods that can control the
+     * wheels to perform different depending on the user input on what the robot should do.*/
     SwerveDrive swerveDrive;
+
+    /**Reference to the driver's gamepad (Xbox) controller.*/
     GamepadEx gamepadEx;
+
+    /**The IMU orientation system of the robot that returns the exact angle (in degrees or in radians) that the robot is currently
+     * facing relative to an orientation that is considered 0 degrees by the IMU.*/
     IMU imu;
 
+    /**Constructs a new {@code SwerveDriveCommand()} with an initialized {@code SwerveDrive}, {@code GamepadEx},
+     * and {@code IMU}.*/
     public SwerveDriveCommand(SwerveDrive swerveDrive, GamepadEx gamepadEx, IMU imu) {
         this.swerveDrive = swerveDrive;
         this.gamepadEx = gamepadEx;
         this.imu = imu;
     }
 
+    /**When this method runs, the {@code SwerveDriveCommand} class will either make the robot stop all of its motors
+     * (if B is pressed on the Xbox controller), reset the yaw of its IMU system (if A was pressed on the Xbox controller), or drive using
+     * its swerve drivetrain (if none of the above were pressed).*/
     @Override
     public void execute() {
         if(gamepadEx.wasJustPressed(GamepadKeys.Button.A)) {
             swerveDrive.resetGyro(imu);
         } else if(gamepadEx.wasJustPressed(GamepadKeys.Button.B)) {
             swerveDrive.stop();
+        } else {
+            swerveDrive.drive();
         }
-        swerveDrive.drive();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboticsInterfaces/Interfaces/RobotVector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboticsInterfaces/Interfaces/RobotVector.java
@@ -3,10 +3,21 @@ package org.firstinspires.ftc.teamcode.RoboticsInterfaces.Interfaces;
 import org.firstinspires.ftc.teamcode.MainTeamcode.Constants;
 
 public class RobotVector {
+    /**The IMU orientation of the robot, in degrees.*/
     public int imuHeadingInDegrees;
-    public double forwardPower, sidePower, turningVector;
-   public  boolean fieldOriented, turningLeft;
+    /**The forward power (y-coordinate) driver input from the driver's controller.*/
+    public double forwardPower,
+    /**The side power (x-coordinate) input from the driver's controller.*/
+    sidePower,
+    /**The power the human driver wants the robot to rotate left/right at.*/
+    turningVector;
+    /**Boolean state of whether the robot will drive field-oriented or robot-oriented.*/
+    public boolean fieldOriented,
+    /**Boolean state of whether the robot is rotating left or right on the spot.*/
+    turningLeft;
 
+    /**Constructs a new {@code RobotVector()} with initialized {@code imuHeadingInDegrees}, {@code forwardPower}, {@code sidePower},
+     * {@code turningVector}, {@code fieldOriented} and {@code turningLeft} variables.*/
     public RobotVector(int imuHeadingInDegrees, double forwardPower, double sidePower, double turningVector, boolean fieldOriented, boolean turningLeft) {
         this.imuHeadingInDegrees = imuHeadingInDegrees;
         this.forwardPower  = forwardPower;
@@ -16,10 +27,13 @@ public class RobotVector {
         this.turningLeft = turningLeft;
     }
 
+    /**Returns value of the {@code imuHeadingInDegrees} variable, but in RADIANS form.*/
     public double degreesToRadians() {
         return Math.toRadians(imuHeadingInDegrees);
     }
 
+    /**Calculates and returns the motor power to give the driving motor depending on the values of {@code forwardPower} and
+     * {@code sidePower} and their relationship.*/
     public double calculateForwardVector() {
         return Math.hypot(forwardPower, sidePower) / Constants.SwerveConstants.vectorScalar;
     }
